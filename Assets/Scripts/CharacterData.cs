d36d1df140a3e5a5324b439428b135700407cf04
Anthony4pf <edeanthony14@gmail.com>
2023-02-23 22:55:17 -0800

    Add Project Files

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class CharacterData : ScriptableObject
+{
+    public new string name;
+    public string[] m_responses;
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI m_dialogue;
+    private string[] m_sentences = {"Default", "Text"};
+    private int m_index;
+
+    public GameObject m_nextButton;
+
+    public CharacterData[] m_characters;
+    public UnityEvent OnEndDialogue;
+    public UnityEvent OnStartDialogue;
+
+    public void StartDialogue(int characterIndex)
+    {
+        OnStartDialogue.Invoke();
+        m_sentences = m_characters[characterIndex].m_responses;
+        m_index = 0;
+        StartCoroutine(TypeDialogue());
+    }
+
+    private void Update()
+    {
+        if (m_dialogue.text == m_sentences[m_index])
+        {
+            m_nextButton.SetActive(true);
+        }
+    }
+
+    IEnumerator TypeDialogue()
+    {
+        foreach (char letter in m_sentences[m_index].ToCharArray())
+        {
+            m_dialogue.text += letter;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    public void NextResponse()
+    {
+        m_nextButton.SetActive(false);
+        if (m_index < m_sentences.Length - 1)
+        {
+            m_index++;
+            m_dialogue.text = "";
+            StartCoroutine(TypeDialogue());
+        }
+        else
+        {
+            m_dialogue.text = "";
+            m_nextButton.SetActive(false);
+            OnEndDialogue.Invoke();
+        }
+    }
+}
